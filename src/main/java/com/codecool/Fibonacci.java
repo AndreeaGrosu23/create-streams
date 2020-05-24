08e188f60fc0792a702c2f4c4715096ceb98266e
Andreea Grosu <andreea.grosu87@gmail.com>
2020-05-24 15:27:44 +0300

    Exercised streams

---
+++ src/main/java/com/codecool/Fibonacci.java
@@ -0,0 +1,16 @@
+package com.codecool;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Fibonacci {
+
+    public List<Integer> generate(int series) {
+        return Stream.iterate(new int[]{0,1}, i -> new int[]{i[1], i[0] + i[1]})
+                    .limit(series)
+                    .map(i->i[0])
+                    .collect(Collectors.toList());
+    }
+
+}
--- src/main/java/com/codecool/Streams.java
+++ src/main/java/com/codecool/Streams.java
@@ -1,5 +1,6 @@
 package com.codecool;
 
+import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.List;
@@ -8,27 +9,35 @@ import java.util.function.UnaryOperator;
 
 public class Streams {
     public static void main(String[] args) {
+
         /*
          * Create a stream out of the following array.
          */
 
         String[] a1 = {"one", "two", "three"};
-        Stream<String> s1 = /* ??? */;
+//        Stream<String> s1 = Arrays.stream(a1);
+        Stream<String> s1 = Arrays.asList(a1).stream();
+        s1.forEach(n -> System.out.println(n));
 
         /*
          * Create a stream containing the Strings "one" , "two" and "three"
          * without using an array
          */
 
-        Stream<String> s2 = /* ??? */;
+        Stream<String> s2 = Stream.of("one", "two", "three");
+        s2.forEach(n -> System.out.println(n));
 
         /*
          * Create a stream using a stream builder.
          */
 
-        Stream.Builder<String> b1 = /* ??? */;
+        Stream.Builder<String> b1 =Stream.builder();
         /* ??? */
-        Stream<String> s3 = /* ??? */;
+        Stream<String> s3 = b1.add(a1[0])
+                                .add(a1[1])
+                                .add(a1[2])
+                                .build();
+        s3.forEach(n -> System.out.println(n));
 
         /*
          * Collect one of the above defined streams into a list.
@@ -36,7 +45,16 @@ public class Streams {
          * HINT: the keyword here is "collect"
          */
 
-        List<String> l1 = /* ??? */;
+        Supplier<Stream<String>> streamSupplier = () -> Stream.of(a1);
+        List<String> l1 = streamSupplier.get().collect(Collectors.toList());
+
+       /*!!!!!Exception in thread "main" java.lang.IllegalStateException:
+        *stream has already been operated upon or closed
+        *For whatever reason, you really want to reuse a Stream,
+        * try the following Supplier solution
+        */
+
+        System.out.println(l1);
 
         /*
          * Streams can be infinite.  We obviously cannot create such a
@@ -54,14 +72,14 @@ public class Streams {
 
         Integer twoToTheZeroth = 1;
         UnaryOperator<Integer> doubler = (Integer x) -> 2 * x;
-        Stream<Integer> s4 = /* ??? */;
-
+        Stream<Integer> s4 = Stream.iterate(twoToTheZeroth, doubler);
+//        s4.limit(10).forEach(n -> System.out.println(n));
         /*
          * Create a stream containing the first ten elements of s4.
          */
 
-        Stream<Integer> s5 = s4. /* ??? */;
-
+        Stream<Integer> s5 = s4.limit(10);
+        s5.forEach(n -> System.out.println(n));
         /*
          * Create a stream containing the elements of the Fibonacci
          * sequence.
@@ -69,7 +87,17 @@ public class Streams {
          * HINT: You will need to create a new class for this.
          */
 
-        Supplier<Integer> fibSupp = new Fibonacci();
-        Stream<Integer> s6 = /* ??? */;
+//        Supplier<Integer> fibSupp = new Fibonacci();
+//        Stream<Integer> s6 = Stream.generate(fibSupp).limit(10).collect(Collectors.toList());
+//        s6.forEach(System.out::println);
+
+        Fibonacci fibonacci = new Fibonacci();
+        List<Integer> listFibo = fibonacci.generate(10);
+        System.out.println(listFibo);
+
+        Stream<List<Integer>> s6 = Arrays.asList(listFibo).stream();
+        s6.forEach(System.out::println);
+
+
     }
 }
